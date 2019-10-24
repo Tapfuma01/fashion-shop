449beefd9176cf2aef7f4cc37536c413040d472c
Tapfuma <enosmhundwa@gmail.com>
2019-10-24 15:54:25 +0200

    added a cart

--- js/app.js
+++ js/app.js
@@ -1,193 +1,209 @@
-// Variables
-const courses = document.querySelector('#courses-list'),
-        shoppingCartContent = document.querySelector('#cart-content tbody')
-        clearCartButton = document.querySelector('#clear-cart');
+// ************************************************
+// Shopping Cart API
+// ************************************************
 
-// Listeners
-loadEventListeners();
-
-
-function loadEventListeners(){
-    // Load all event listeners for this project
-
-    // When a new course is added
-    courses.addEventListener('click', buyCourse);
-
-    // when the remove button is clicked
-    shoppingCartContent.addEventListener('click', removeCourse);
-
-    // Clear the cart button
-    clearCartButton.addEventListener('click', clearCart);
-
-    // Document ready
-    document.addEventListener('DOMContentLoaded', getFromLocalStorage);
-}
-
-// Functions
-// Remember to use delegation you have to pass the event (e)
-function buyCourse(e){
-    /* to stop page jumping to top every time you click
-    use e.preventDefault()
-    this is because browser tries to open new page every time
-    you click a link */
-    // PREVENT PAGE JUMPING TO TOP ON LINK CLICK
-    e.preventDefault();
-
-    // Use delegation to find the course that was added into shopping cart
-    if(e.target.classList.contains('add-to-cart')){
-        // read course values using parent element
-        const course = e.target.parentElement.parentElement;
-
-        // read the values
-        getCourseInfo(course);
-    }
-}
-
-//Reads the HTML information of the selected course
-function getCourseInfo(course){
-    // Create an Object with Course Data
-    const courseInfo = {
-        image: course.querySelector('img').src,
-        title: course.querySelector('h4').textContent,
-        price: course.querySelector('.price span').textContent,
-        id: course.querySelector('a').getAttribute('data-id')
+var shoppingCart = (function() {
+    // =============================
+    // Private methods and propeties
+    // =============================
+    cart = [];
+    
+    // Constructor
+    function Item(name, price, count) {
+      this.name = name;
+      this.price = price;
+      this.count = count;
     }
     
-    // Insert into shopping cart
-    addIntoCart(courseInfo);
-}
-
-// Display selected course into shopping cart
-function addIntoCart(course){
-    // create a <tr></tr> in table in shopping cart
-    const row = document.createElement('tr');
-
-    // Build template of passed in course info in table row
-    row.innerHTML = `
-        <tr>
-            <td>
-                <img src="${course.image}" width=100 font-size:60px;>
-            </td>
-            <td>
-                ${course.title}
-            </td>
-            <td>
-                ${course.price}
-            </td>
-            <td>
-                <a href = "#" class="remove" data-id="${course.id}">X</a>
-            </td>
-        </tr>
-    `;
-    // Add into the shopping cart
-    shoppingCartContent.appendChild(row);
-
-    // Add course into local storage
-    saveIntoStorage(course);
-}
-
-// Add the courses into the local storage
-function saveIntoStorage(course){
-    let courses = getCoursesFromStorage();
-
-    // add the course into the array
-    courses.push(course);
-
-    // since storage only saves strings, we need to convert JSON into
-    // string
-    localStorage.setItem('courses', JSON.stringify(courses));
-}
-
-// Get the contents from the storage
-function getCoursesFromStorage(){
-    let courses;
-
-    // if key exists in storage then we get value otherwise
-    // create and empty array
-    if(localStorage.getItem('courses') === null){
-        courses = [];
-    } else {
-        courses = JSON.parse(localStorage.getItem('courses')); 
+    // Save cart
+    function saveCart() {
+      sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
     }
-    return courses;
-}
-
-// remove course from the dom
-function removeCourse(e){
-    let course, courseId;
-    // remove from the DOM
-    if(e.target.classList.contains('remove')){
-        e.target.parentElement.parentElement.remove();
-        course = e.target.parentElement.parentElement;
-        courseId = course.querySelector('a').getAttribute('data-id');
+    
+      // Load cart
+    function loadCart() {
+      cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
     }
-    console.log(courseId);
-    // Remove item from local storage
-    removeCourseLocalStorage(courseId);
-}
-
-// remove from local storage
-function removeCourseLocalStorage(id){
-    // get the local storage data
-    let coursesLS = getCoursesFromStorage();
-
-    // loop through the array and find the index to remove
-    coursesLS.forEach(function(courseLS, index){
-        if(courseLS.id === id){
-            coursesLS.splice(index, 1);
+    if (sessionStorage.getItem("shoppingCart") != null) {
+      loadCart();
+    }
+    
+  
+    // =============================
+    // Public methods and propeties
+    // =============================
+    var obj = {};
+    
+    // Add to cart
+    obj.addItemToCart = function(name, price, count) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart[item].count ++;
+          saveCart();
+          return;
         }
-    });
-
-    // Add the rest of the array into local storage
-    localStorage.setItem('courses', JSON.stringify(coursesLS));
-}
-
-// Clears the shopping cart
-function clearCart(){
-    // Method 1: overwrite html in table body with empty string
-    //shoppingCartContent.innerHTML = '';
-
-    // Method 2: Reccomended way
-    // Loop while there is a first child in shopping cart
-    while(shoppingCartContent.firstChild){
-        shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+      }
+      var item = new Item(name, price, count);
+      cart.push(item);
+      saveCart();
     }
-
-    // Clear from local storage
-    clearLocalStorage();
-}
-
-// Clears the whole local storage
-function clearLocalStorage(){
-    localStorage.clear();
-}
-
-// Loads when document is ready and print courses into shopping cart
-function getFromLocalStorage(){
-    let coursesLS = getCoursesFromStorage();
-
-    // LOOP through the courses and print into the cart
-    coursesLS.forEach(function(course){
-        // create the <tr></tr>
-        const row = document.createElement('tr');
-
-        // print the content
-        row.innerHTML = `
-            <tr>
-                <td>
-                    <img src="${course.image}" width=100>
-                </td>
-                <td>
-                    ${course.title}
-                </td>
-                <td>
-                    ${course.price}
-                </td>
-                <td>
-                    <a href = "#" class="remove" data-id="${course.id}">X</a>
-                </td>
-            </tr>
-        `;
-        shoppingCartContent.appendChild(row);
-    })
-}
+    // Set count from item
+    obj.setCountForItem = function(name, count) {
+      for(var i in cart) {
+        if (cart[i].name === name) {
+          cart[i].count = count;
+          break;
+        }
+      }
+    };
+    // Remove item from cart
+    obj.removeItemFromCart = function(name) {
+        for(var item in cart) {
+          if(cart[item].name === name) {
+            cart[item].count --;
+            if(cart[item].count === 0) {
+              cart.splice(item, 1);
+            }
+            break;
+          }
+      }
+      saveCart();
+    }
+  
+    // Remove all items from cart
+    obj.removeItemFromCartAll = function(name) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart.splice(item, 1);
+          break;
+        }
+      }
+      saveCart();
+    }
+  
+    // Clear cart
+    obj.clearCart = function() {
+      cart = [];
+      saveCart();
+    }
+  
+    // Count cart 
+    obj.totalCount = function() {
+      var totalCount = 0;
+      for(var item in cart) {
+        totalCount += cart[item].count;
+      }
+      return totalCount;
+    }
+  
+    // Total cart
+    obj.totalCart = function() {
+      var totalCart = 0;
+      for(var item in cart) {
+        totalCart += cart[item].price * cart[item].count;
+      }
+      return Number(totalCart.toFixed(2));
+    }
+  
+    // List cart
+    obj.listCart = function() {
+      var cartCopy = [];
+      for(i in cart) {
+        item = cart[i];
+        itemCopy = {};
+        for(p in item) {
+          itemCopy[p] = item[p];
+  
+        }
+        itemCopy.total = Number(item.price * item.count).toFixed(2);
+        cartCopy.push(itemCopy)
+      }
+      return cartCopy;
+    }
+  
+    // cart : Array
+    // Item : Object/Class
+    // addItemToCart : Function
+    // removeItemFromCart : Function
+    // removeItemFromCartAll : Function
+    // clearCart : Function
+    // countCart : Function
+    // totalCart : Function
+    // listCart : Function
+    // saveCart : Function
+    // loadCart : Function
+    return obj;
+  })();
+  
+  
+  // *****************************************
+  // Triggers / Events
+  // ***************************************** 
+  // Add item
+  $('.add-to-cart').click(function(event) {
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+  });
+  
+  // Clear items
+  $('.clear-cart').click(function() {
+    shoppingCart.clearCart();
+    displayCart();
+  });
+  
+  
+  function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = "";
+    for(var i in cartArray) {
+      output += "<tr>"
+        + "<td>" + cartArray[i].name + "</td>" 
+        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+        + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + " = " 
+        + "<td>" + cartArray[i].total + "</td>" 
+        +  "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+  }
+  
+  // Delete item button
+  
+  $('.show-cart').on("click", ".delete-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+  })
+  
+  
+  // -1
+  $('.show-cart').on("click", ".minus-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCart(name);
+    displayCart();
+  })
+  // +1
+  $('.show-cart').on("click", ".plus-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.addItemToCart(name);
+    displayCart();
+  })
+  
+  // Item count input
+  $('.show-cart').on("change", ".item-count", function(event) {
+     var name = $(this).data('name');
+     var count = Number($(this).val());
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+  });
+  
+  displayCart();
+  
